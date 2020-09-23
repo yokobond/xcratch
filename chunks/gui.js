933fab8d14f638454dee0fb74e44eee8360aeb11
yokobond <yokobond@users.noreply.github.com>
2020-09-23 02:12:29 +0000

    deploy: 786704e610dbe814fc3dc198157aeac63bb4651c

--- chunks/gui.js
+++ chunks/gui.js
@@ -147,7 +147,7 @@ function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "functi
 
 
 var onClickLogo = function onClickLogo() {
-  window.location = 'https://scratch.mit.edu';
+  window.location = 'https://github.com/yokobond/xcratch';
 };
 
 var handleTelemetryModalCancel = function handleTelemetryModalCancel() {
